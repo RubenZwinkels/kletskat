d8a6f20c2d98051641b73dabac710d21761c4184
RubenZwinkels <s1149334@student.hsleiden.nl>
2024-09-21 13:24:02 +0200

    init swift project

---
+++ KletsKatApp/KletsKatApp/KletsKatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  KletsKatAppApp.swift
+//  KletsKatApp
+//
+//  Created by Ruben Zwinkels on 21/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct KletsKatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
