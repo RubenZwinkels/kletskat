8a6b62ce746a55a3402efb7cf969ad08d6c55894
RubenZwinkels <s1149334@student.hsleiden.nl>
2024-09-22 19:28:16 +0200

    kleuren pallet toegevoegd

--- KletsKatApp/KletsKatApp/View/KletsKatAppApp.swift
+++ KletsKatApp/KletsKatApp/View/KletsKatAppApp.swift
@@ -2,9 +2,10 @@
 //  KletsKatAppApp.swift
 //  KletsKatApp
 //
-//  Created by Ruben Zwinkels on 21/09/2024.
+//  Created by Ruben Zwinkels on 22/09/2024.
 //
 
+import Foundation
 import SwiftUI
 
 @main
---
+++ KletsKatApp/KletsKatApp/View/LoginSignupView.swift
@@ -0,0 +1,8 @@
+//
+//  LoginSignupView.swift
+//  KletsKatApp
+//
+//  Created by Ruben Zwinkels on 22/09/2024.
+//
+
+import Foundation
