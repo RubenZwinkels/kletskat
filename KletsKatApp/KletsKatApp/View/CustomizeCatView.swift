f4b6f155a8f7afdb210f9ce6032b7af8a6bde639
RubenZwinkels <s1149334@student.hsleiden.nl>
2024-10-11 11:17:21 +0200

    kat customization kleur kiezen + naam veranderen

--- KletsKatApp/KletsKatApp/View/CustomizeCatView.swift
+++ KletsKatApp/KletsKatApp/View/CustomizeCatView.swift
@@ -8,29 +8,54 @@ struct CustomizeCatView: View {
     @State private var catName: String = "Simba"
     @State private var catPersonality: Personality = .happy
     
-    let availableColors: [Color] = [.orange, .yellow, .white, .black, .gray, .brown]
+    let availableCatColors: [Color] = [.orange, .yellow, .white, .black, .gray, .brown]
     
     var body: some View {
-        ZStack{
+        ZStack {
             Color.background.ignoresSafeArea() // achtergrond
             VStack {
-                CatView(catColor: catColor, eyeColor: eyeColor)
-                HStack {
-                    ForEach(availableColors, id: \.self) { color in
-                        Circle()
-                            .fill(color)
-                            .frame(width: 50, height: 50)
-                            .onTapGesture {
-                                catColor = color
-                            }
-                            .overlay(
-                                Circle()
-                                    .stroke(catColor == color ? Color.black : Color.clear, lineWidth: 3)
-                            )
-                    }
-                }
-                .padding()
+                TextField("Voer de naam van je kat in", text: $catName) // kat naam
+                    .font(.largeTitle)
+                    .padding()
+                    .multilineTextAlignment(.center)
+                    .background(.primairy.opacity(0.8))
+                    .cornerRadius(10)
+                    .padding(.horizontal)
+                    .padding(.bottom, 30)
 
+                // Kat tonen
+                CatView(catColor: catColor, eyeColor: eyeColor)
+                    .padding(.bottom, 30)
+                
+                // Kat kleur kiezen
+                VStack {
+                    Text("Selecteer de kleur van je kat")
+                        .font(.title2)
+                        .padding(.top, 20)
+                    HStack {
+                        ForEach(availableCatColors, id: \.self) { color in
+                            Circle()
+                                .fill(color)
+                                .frame(width: 50, height: 50)
+                                .onTapGesture { // Oogkleur omzetten
+                                    catColor = color
+                                    if color == .black {
+                                        eyeColor = .gray // Ogen worden grijs bij zwarte kat
+                                    } else {
+                                        eyeColor = .black
+                                    }
+                                }
+                                .overlay(
+                                    Circle()
+                                        .stroke(catColor == color ? Color.black : Color.clear, lineWidth: 3)
+                                )
+                        }
+                    }
+                    .padding()
+                }
+                .background(
+                    RoundedRectangle(cornerRadius: 20) // achtergrondje voor selectbox
+                        .fill(.primairy))
             }
             .onAppear {
                 self.catColor = catController.catModel.color
