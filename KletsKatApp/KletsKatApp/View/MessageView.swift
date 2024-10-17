622e3b58bc27e0485fdeae4b87ec8d891283019a
Ruben Zwinkels <rubenzwinkels05@gmail.com>
2024-10-17 20:38:38 +0200

    [fix] text wordt niet meer afgekapt door bubbel vorm

--- KletsKatApp/KletsKatApp/View/MessageView.swift
+++ KletsKatApp/KletsKatApp/View/MessageView.swift
@@ -1,38 +1,37 @@
-//
-//  MessageView.swift
-//  KletsKatApp
-//
-//  Created by Ruben Zwinkels on 16/10/2024.
-//
 import SwiftUI
 
 struct MessageView: View {
     var message: Message
+    
     var body: some View {
         Group {
             if message.isUser {
                 HStack {
                     Spacer()
                     Text(message.content)
-                        .padding()
-                        .background(Color.blue)
+                        .padding(15)
+                        .background(.primairy)
                         .foregroundColor(Color.white)
-                        .clipShape(Capsule())
+                        .clipShape(RoundedRectangle(cornerRadius: 20))
+                        .fixedSize(horizontal: false, vertical: true) // Laat de tekst over meerdere regels lopen
                 }
+                .padding(.horizontal)
             } else {
                 HStack {
                     Text(message.content)
-                        .padding()
-                        .background(Color.black)
+                        .padding(15)
+                        .background(.secondairy)
                         .foregroundColor(Color.white)
-                        .clipShape(Capsule())
+                        .clipShape(RoundedRectangle(cornerRadius: 20))
+                        .fixedSize(horizontal: false, vertical: true)
                     Spacer()
                 }
+                .padding(.horizontal)
             }
         }
     }
 }
 
 #Preview {
-    ContentView()
+    MessageView(message: Message(content: "Dit is een voorbeeld van een lange tekst die moet worden gesplitst in meerdere regels zonder afgekapt te worden door de vorm van de tekstbubbel.", isUser: true))
 }
