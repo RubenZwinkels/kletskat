c6b94e0efbe8054d510e7c9d7b53c5812bd8aef4
Ruben Zwinkels <rubenzwinkels05@gmail.com>
2024-10-17 20:54:16 +0200

    base widget toegevoegd

---
+++ KletsKatApp/KletsKatWidget/KletsKatWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  KletsKatWidgetBundle.swift
+//  KletsKatWidget
+//
+//  Created by Ruben Zwinkels on 17/10/2024.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct KletsKatWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        KletsKatWidget()
+    }
+}
