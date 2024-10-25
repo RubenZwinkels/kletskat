548d30def1e8b65469721116928ada19a076fc6e
Ruben Zwinkels <rubenzwinkels05@gmail.com>
2024-10-25 19:47:29 +0200

    home knop toegevoegd zodat de homeview wordt geupodate

--- KletsKatApp/KletsKatApp/View/ChatView.swift
+++ KletsKatApp/KletsKatApp/View/ChatView.swift
@@ -1,49 +1,72 @@
-//
-//  ChatView.swift
-//  KletsKatApp
-//
-//  Created by Ruben Zwinkels on 16/10/2024.
-//
-
 import SwiftUI
 import OpenAI
 import Foundation
 
 struct ChatView: View {
     @StateObject var chatController: ChatController = .init()
-    @State var string: String = ""
+    @State private var string: String = ""
+
     var body: some View {
-        ZStack{
-            Color.background.ignoresSafeArea() // achtergrond
-            VStack {
-                ScrollView {
-                    ForEach(chatController.messages) {
-                        message in
-                        MessageView(message: message)
-                            .padding(5)
+        NavigationView {
+            ZStack {
+                Color.background.ignoresSafeArea() // achtergrond
+                VStack {
+                    ScrollView {
+                        ForEach(chatController.messages) { message in
+                            MessageView(message: message)
+                                .padding(5)
+                        }
+                        Spacer()
                     }
-                }
-                Divider()
-                HStack {
-                    TextField("Message...", text: self.$string, axis: .vertical)
-                        .padding(5)
-                        .background(Color.gray.opacity(0.1))
-                        .cornerRadius(15)
-                    Button {
-                        self.chatController.sendNewMessage(content: string)
-                        string = ""
-                    } label: {
-                        Image(systemName: "paperplane.circle.fill")
-                            .foregroundColor(.highlight)
-                            .font(.system(size: 30))
+                    .onTapGesture {
+                        hideKeyboard()
                     }
+
+                    Divider()
+
+                    HStack {
+                        TextField("Message...", text: self.$string, axis: .vertical)
+                            .padding(10)
+                            .background(Color.gray.opacity(0.1))
+                            .cornerRadius(15)
+
+                        Button {
+                            if !string.isEmpty {
+                                self.chatController.sendNewMessage(content: string)
+                                string = ""
+                            }
+                        } label: {
+                            Image(systemName: "paperplane.circle.fill")
+                                .foregroundColor(.highlight)
+                                .font(.system(size: 30))
+                        }
+                    }
+                    .padding()
                 }
-                .padding()
             }
+            .navigationBarItems(
+                leading: NavigationLink(destination: HomeView()) {
+                    HStack {
+                        Image(systemName: "arrow.backward.circle.fill")
+                            .foregroundColor(.blue)
+                            .font(.system(size: 20))
+                        Text("Home")
+                            .foregroundColor(.blue)
+                    }
+                }
+            )
+            .navigationBarTitle("Chat", displayMode: .inline)
         }
     }
 }
 
+// Functie om het toetsenbord te verbergen
+extension View {
+    func hideKeyboard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+}
+
 #Preview {
-    ChatView(string: "lorum ipsum")
+    ChatView()
 }
