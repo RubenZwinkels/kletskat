f6f5f8d8746ee1f5546744a5846a891e73a81d99
Ruben Zwinkels <rubenzwinkels05@gmail.com>
2024-10-30 11:50:46 +0100

    verwijder todo item visueel

--- KletsKatApp/KletsKatApp/View/todo/TodoItemView.swift
+++ KletsKatApp/KletsKatApp/View/todo/TodoItemView.swift
@@ -4,11 +4,11 @@ struct TodoItemView: View {
     @ObservedObject var todoController: TodoController = TodoController()
     var todoItem: TodoItem
 
-    // @State voor het bijhouden van de toggle-status en laadstatus
     @State private var isChecked: Bool
-    @State private var isLoading: Bool = false // Voor het bijhouden van de laadstatus
+    @State private var isLoading: Bool = false
     private var bigMode: Bool
-    
+    @Environment(\.presentationMode) var presentationMode // Voeg deze lijn toe
+
     init(todoController: TodoController, todoItem: TodoItem, bigMode: Bool) {
         self.todoController = todoController
         self.todoItem = todoItem
@@ -17,27 +17,23 @@ struct TodoItemView: View {
     }
 
     var body: some View {
-        ZStack{
-            Color.background.ignoresSafeArea() // achtergrond
+        ZStack {
+            Color.background.ignoresSafeArea()
             VStack {
                 HStack {
                     Toggle(isOn: Binding<Bool>(
-                        get: {
-                            isChecked
-                        },
+                        get: { isChecked },
                         set: { newValue in
-                            isChecked = newValue // Lokaal bijwerken om de UI direct te updaten
-                            isLoading = true // Begin met laden
+                            isChecked = newValue
+                            isLoading = true
                             
-                            // Synchroniseer de wijziging met de backend
                             todoController.toggleTodoItem(todoItem.id) { success in
                                 DispatchQueue.main.async {
-                                    isLoading = false // Stop met laden
+                                    isLoading = false
                                     if success {
                                         print("Todo item toggle succesvol")
                                     } else {
                                         print("Fout bij het togglen van todo item")
-                                        // Reset de toggle naar de oorspronkelijke waarde bij een fout
                                         isChecked.toggle()
                                     }
                                 }
@@ -52,47 +48,48 @@ struct TodoItemView: View {
                     .toggleStyle(SwitchToggleStyle(tint: .highlight))
                     .padding()
                     
-                    // Laadindicator tonen als isLoading waar is
                     if isLoading {
                         ProgressView()
                             .progressViewStyle(CircularProgressViewStyle(tint: .highlight))
-                            .frame(width: 20, height: 20) // Pas de grootte aan zoals nodig
+                            .frame(width: 20, height: 20)
                     }
                 }
                 .padding()
-                .background(Color(.primairy)) // Achtergrondkleur voor het item
-                .cornerRadius(8) // Ronde hoeken
-                .shadow(radius: 2) // Schaduw voor diepte
+                .background(Color(.primairy))
+                .cornerRadius(8)
+                .shadow(radius: 2)
                 
-                // Wanneer de gebruiker erop klikt, tonen we extra informatie
                 if bigMode {
-                    VStack{
-                        //beschijving en datum
-                        VStack(alignment: .leading) {
-                            Text("Beschrijving: \(todoItem.description)")
-                                .font(.subheadline)
-                                .foregroundColor(.black)
-                                .padding(.top, 4)
-                            
-                            Text("Gemaakt op: \(todoItem.creationDate!)")
-                                .font(.footnote)
-                                .foregroundColor(.black)
-                        }
-                        .padding(.top)
-                        .padding(.horizontal)
-                        .background(Color(.secondairy)) // Achtergrondkleur voor de beschrijving
-                        .cornerRadius(8)
-                        .shadow(radius: 1)
+                    VStack(alignment: .leading) {
+                        Text("Beschrijving: \(todoItem.description)")
+                            .font(.subheadline)
+                            .foregroundColor(.black)
+                            .padding(.top, 4)
+                        
+                        Text("Gemaakt op: \(todoItem.creationDate!)")
+                            .font(.footnote)
+                            .foregroundColor(.black)
                     }
-                    //delete knop
-                    Button("Verwijder", systemImage: "trash.circle.fill"){
-                        todoController.deleteTodoItem(todoItem.id){ success in
-                            print("todoitem verwijderd")
+                    .padding(.top)
+                    .padding(.horizontal)
+                    .background(Color(.secondairy))
+                    .cornerRadius(8)
+                    .shadow(radius: 1)
+                    
+                    Button(action: {
+                        todoController.deleteTodoItem(todoItem.id) { success in
+                            DispatchQueue.main.async { // hierdoor wordt dit process uitgevoerd op main thread (anders crasht het)
+                                if success {
+                                    presentationMode.wrappedValue.dismiss() // sluit de view
+                                }
+                            }
                         }
+                    }) {
+                        Label("Verwijder", systemImage: "trash.circle.fill")
                     }
                 }
             }
-            .padding(.horizontal) // Padding voor de hele view
+            .padding(.horizontal)
         }
     }
 }
